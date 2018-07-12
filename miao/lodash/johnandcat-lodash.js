0f8bff4135e0e2c312b77cd438eaacb4f43a06e3
johncat <283623681@qq.com>
2018-07-12 11:13:53 +0800

    lodash

--- miao/lodash/johnandcat-lodash.js
+++ miao/lodash/johnandcat-lodash.js
@@ -1,4 +1,4 @@
-var guts111 = {
+var johnandcat = {
     //将数组（array）拆分成多个 size 长度的区块，并将这些区块组成一个新数组。 如果array无法被分割成全部等长的区块，那么最后剩余的元素将组成一个区块。
     chunk: function(array, size = 1) {
         var result = [];
